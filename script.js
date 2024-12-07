c65d3705f6eeda153233d9fc68a99877b4e33b6f
Glitch (glitch-hello-website) <none>
2024-12-07 21:42:55 +0000

    🎅🎥 Checkpoint ./index.html:11388337/1009 ./script.js:11388337/1447

--- script.js
+++ script.js
@@ -1,98 +1,44 @@
-import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
-import safeHtml from 'https://cdn.jsdelivr.net/npm/safe-html@1.0.0/safe-html.min.js';
+import safeHtml from 'https://cdn.jsdelivr.net/npm/safe-html@1.0.0/+esm';
 
-export class Posts extends LitElement {
-  static properties = {
-    url: {},
-    posts: []
-  };
-
-  static styles = css`
-  .post-list {
-    font-family: Arial, sans-serif;
-  }
-
-  h2 {
-    color: #333;
-    font-family: Arial, sans-serif;
-  }
-
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  li {
-    margin-bottom: 20px;
-    border-bottom: 1px solid #ddd;
-    padding-bottom: 15px;
-  }
-
-  h3 {
-    color: #0073e6;
-  }
-
-  .error {
-    color: red;
-  }
-
-  a {
-    color: #0073e6;
-    text-decoration: none;
-  }
-
-  a:hover {
-    text-decoration: underline;
-  }`;
-
-  constructor() {
-    super();
-    this.url = '';
-    this.posts = [];
-  }
+const properties = {
+  url: {}, 
+  posts: []
+};
   
-  async handleFormSubmit(event) {
-    event.preventDefault();
-    
-    const formData = new FormData(event.target)
-    const formValues = Object.fromEntries(formData.entries())
-    const url = `${ formValues.name }/wp-json/wp/v2/posts`
-    
-    try {
-      const response = await fetch(url)
-      if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      const data = await response.json();
-      this.posts = data;
-    }
-    catch (error) {
-      console.log(`Error: ${error.message}`);
-    }
-  }
+async function handleFormSubmit(event) {
+  event.preventDefault();
 
-  render() {
-    let posts = '';
-    if(this.posts && this.posts.length > 0) {
-      posts = html`<h2>Latest Posts</h2>
-      <ul class="post-list">
-      ${this.posts.map((post) =>
-          html`<li>
-          <h3>${post.title.rendered}</h3>
-            <p>${unsafeHTML(post.excerpt.rendered)}</p>
-          </li>`
-        )}
-      </ul>`
+  const formData = new FormData(event.target)
+  const formValues = Object.fromEntries(formData.entries())
+  const url = `${ formValues.name }/wp-json/wp/v2/posts`
+
+  try {
+    const response = await fetch(url)
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
     }
-    
-    return html`
-       <form @submit=${this.handleFormSubmit}>
-        <label for="name">Enter WordPress URL:</label>
-        <input type="text" id="name" name="name" required />
-        
-        <input type="submit" value="Submit" />
-      </form>
-      ${posts}`;
+    const data = await response.json();
+    this.posts = data;
+    render();
+  }
+  catch (error) {
+    console.log(`Error: ${error.message}`);
   }
 }
-customElements.define('wp-posts', Posts);
+
+function render() {
+  let posts = '';
+  if(this.posts && this.posts.length > 0) {
+    posts = `<h2>Latest Posts</h2>
+    <ul class="post-list">`;
+    posts.forEach((post) => {
+        posts += `<li>
+        <h3>${safeHtml(post.title.rendered)}</h3>
+          <p>${safeHtml(post.excerpt.rendered)}</p>
+        </li>`
+      });
+    </ul>`
+  }
+
+  console.log(posts);
+}
